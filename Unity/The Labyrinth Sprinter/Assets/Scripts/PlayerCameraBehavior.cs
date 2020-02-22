0dfd3c412c5ab8ef4d65f0ae57b660c8f3510a64
William Pritschau <pritscwh@mail.uc.edu>
2020-02-22 15:05:20 -0500

    added simple camera

---
+++ Unity/The Labyrinth Sprinter/Assets/Scripts/PlayerCameraBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraBehavior : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBodyTransf;
+    public Transform playerCameraTransf;
+
+    float xRotation = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        playerCameraTransf.localRotation = Quaternion.Euler(new Vector3(xRotation, 0.0f, 0.0f));
+        playerBodyTransf.Rotate(Vector3.up, mouseX);
+    }
+}
---
+++ Unity/The Labyrinth Sprinter/Assets/Scripts/PlayerMovementBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementBehavior : MonoBehaviour
+{
+    public float maxSpeed = 0.5f;
+    public Transform PlayerBodyTransf;
+    //CapsuleCollider PlayerBodyCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get player input
+        bool xPos = Input.GetButton("Forward");
+        bool xNeg = Input.GetButton("Backward");
+        bool yPos = Input.GetButton("Right");
+        bool yNeg = Input.GetButton("Left");
+
+        // Create a "Desired" Movement Vector
+        Vector3 movementIn = new Vector3(yPos && yNeg ? 0.0f : yPos ? 1.0f : yNeg ? -1.0f : 0.0f, 0.0f, xPos && xNeg ? 0.0f : xPos ? 1.0f : xNeg ? -1.0f : 0.0f);
+        movementIn = maxSpeed * Time.deltaTime * Vector3.Normalize(movementIn);
+        Debug.Log("Moving x: " + movementIn.x + " z: " + movementIn.z);
+
+
+        // Check what components are viable
+
+        // Move based off those components
+        PlayerBodyTransf.Translate(movementIn);
+    }
+}
